a32fc86744e15c6534d706f7977dcbe84d0953c2
zhuyijing168 <zhuyijing168@163.com>
2021-01-03 17:28:21 +0800

    cheatcard

---
+++ miniprogram/components/cheatcard/index.js
@@ -0,0 +1,26 @@
+// components/cheatcard/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    codeText: {
+      type: String,
+      value: 'set -x',
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
